bfd9bc302125917d70d289e246ff5a6c0535684f
Ali El <87979582+ali20051@users.noreply.github.com>
2023-03-11 16:43:59 +0100

    Update dash.js

--- routes/dash.js
+++ routes/dash.js
@@ -5,7 +5,53 @@ const db = nano.use("users");
 
 
 router.get('/dashboard', (req , res) => {
-      res.render('dash', { displayname: "Jack", amount: 50000, id: "A123456789", history: [] });
+      res.render('dash', { displayname: "Jack", amount: 50000, id: "A123456789", history: [
+      {
+      "where": "Apple"
+    },
+    {
+      "where": "Wallmart"
+    },
+    {
+      "where": "Netflix"
+    },
+    {
+      "where": "HBO+"
+    },
+    {
+      "where": "Margan co."
+    },
+    {
+      "where": "Orange"
+    },
+    {
+      "where": "Maroc Telecom"
+    },
+    {
+      "where": "elba.com"
+    },
+    {
+      "where": "Best Buy"
+    },
+    {
+      "where": "Costo"
+    },
+    {
+      "where": "1$ Store"
+    },
+    {
+      "where": "Donation To Charety"
+    },
+    {
+      "where": "Play Store"
+    },
+    {
+      "where": "Google"
+    },
+    {
+      "where": "Baby Store"
+    }
+   ] });
 });
 
 
